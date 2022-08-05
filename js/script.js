281c8dd99bb2edc68d68174114f27e83315d594b
turdaliyevjasur02@gmail.com <turdaliyevjasur02@gmail.com>
2022-08-05 19:01:24 -0400

    git all

---
+++ js/script.js
@@ -0,0 +1,14 @@
+var intro = alert("Diskotekamizga Xush kelibsiz");
+var first_name = prompt("Ismingiz nima?", "Jasur").trim();
+var age = prompt("Yoshingiz nechida?",).trim();
+
+
+if (age >= 18) {
+  alert('marhamat kirishingiz mumkin')
+} else{
+  alert('siz kira olmaysiz')
+}
+
+console.log (`Malumotlaringiz: 
+Foydalanuvchining ismi:${first_name} 
+Foydalanuvchining yoshi:${age}`);
